6746d3e2734c35493538383430848185d5843436
Tom Kaye <tom@canewdonkayes.plus.com>
2016-07-21 16:27:18 +0100

    Aesthetic changes for calendar.

--- LectureCalendar.php
+++ LectureCalendar.php
@@ -12,92 +12,92 @@
                 <input type="button" class="Back Button" onclick="window.location = 'index.html';" value="Back to Main Page">
             </div>
             <br/>
-            <div>
+            <div class="Sub-Div">
                 <h3>Year Three</h3>
                 <select class="Odd-Even-Select List">
                     <option class="Odd" value="Odd" selected="selected">Odd Week</option>
                     <option class="Even" value="Even">Even Week</option>
                 </select>
                 <table class="Calendar-Table">
-                    <tr class="Day-Row">
-                        <th></th>
-                        <th>Monday</th>
-                        <th>Tuesday</th>
-                        <th>Wednesday</th>
-                        <th>Thursday</th>
-                        <th>Friday</th>
+                    <tr>
+                        <th class="Header Cell"></th>
+                        <th class="Header Cell">Monday</th>
+                        <th class="Header Cell">Tuesday</th>
+                        <th class="Header Cell">Wednesday</th>
+                        <th class="Header Cell">Thursday</th>
+                        <th class="Header Cell">Friday</th>
                     </tr>
-                    <tr class="9-Row">
-                        <th class="Day-Row">9AM</th>
-                        <td id="9Mo"></td>
-                        <td id="9Tu"></td>
-                        <td id="9We"></td>
-                        <td id="9Th"></td>
-                        <td id="9Fr"></td>
+                    <tr>
+                        <th class="Header Cell">9AM</th>
+                        <td id="9Mo" class="Cell"></td>
+                        <td id="9Tu" class="Cell"></td>
+                        <td id="9We" class="Cell"></td>
+                        <td id="9Th" class="Cell"></td>
+                        <td id="9Fr" class="Cell"></td>
                     </tr>
-                    <tr class="10-Row">
-                        <th class="Day-Row">10AM</th>
-                        <td id="10Mo"></td>
-                        <td id="10Tu"></td>
-                        <td id="10We"></td>
-                        <td id="10Th"></td>
-                        <td id="10Fr"></td>
+                    <tr>
+                        <th class="Header Cell">10AM</th>
+                        <td id="10Mo" class="Cell"></td>
+                        <td id="10Tu" class="Cell"></td>
+                        <td id="10We" class="Cell"></td>
+                        <td id="10Th" class="Cell"></td>
+                        <td id="10Fr" class="Cell"></td>
                     </tr>
-                    <tr class="11-Row">
-                        <th class="Day-Row">11AM</th>
-                        <td id="11Mo"></td>
-                        <td id="11Tu"></td>
-                        <td id="11We"></td>
-                        <td id="11Th"></td>
-                        <td id="11Fr"></td>
+                    <tr>
+                        <th class="Header Cell">11AM</th>
+                        <td id="11Mo" class="Cell"></td>
+                        <td id="11Tu" class="Cell"></td>
+                        <td id="11We" class="Cell"></td>
+                        <td id="11Th" class="Cell"></td>
+                        <td id="11Fr" class="Cell"></td>
                     </tr>
-                    <tr class="12-Row">
-                        <th class="Day-Row">12PM</th>
-                        <td id="12Mo"></td>
-                        <td id="12Tu"></td>
-                        <td id="12We"></td>
-                        <td id="12Th"></td>
-                        <td id="12Fr"></td>
+                    <tr>
+                        <th class="Header Cell">12PM</th>
+                        <td id="12Mo" class="Cell"></td>
+                        <td id="12Tu" class="Cell"></td>
+                        <td id="12We" class="Cell"></td>
+                        <td id="12Th" class="Cell"></td>
+                        <td id="12Fr" class="Cell"></td>
                     </tr>
-                    <tr class="13-Row">
-                        <th class="Day-Row">1PM</th>
-                        <td id="13Mo"></td>
-                        <td id="13Tu"></td>
-                        <td id="13We"></td>
-                        <td id="13Th"></td>
-                        <td id="13Fr"></td>
+                    <tr>
+                        <th class="Header Cell">1PM</th>
+                        <td id="13Mo" class="Cell"></td>
+                        <td id="13Tu" class="Cell"></td>
+                        <td id="13We" class="Cell"></td>
+                        <td id="13Th" class="Cell"></td>
+                        <td id="13Fr" class="Cell"></td>
                     </tr>
-                    <tr class="14-Row">
-                        <th class="Day-Row">2PM</th>
-                        <td id="14Mo"></td>
-                        <td id="14Tu"></td>
-                        <td id="14We"></td>
-                        <td id="14Th"></td>
-                        <td id="14Fr"></td>
+                    <tr>
+                        <th class="Header Cell">2PM</th>
+                        <td id="14Mo" class="Cell"></td>
+                        <td id="14Tu" class="Cell"></td>
+                        <td id="14We" class="Cell"></td>
+                        <td id="14Th" class="Cell"></td>
+                        <td id="14Fr" class="Cell"></td>
                     </tr>
-                    <tr class="15-Row">
-                        <th class="Day-Row">3PM</th>
-                        <td id="15Mo"></td>
-                        <td id="15Tu"></td>
-                        <td id="15We"></td>
-                        <td id="15Th"></td>
-                        <td id="15Fr"></td>
+                    <tr>
+                        <th class="Header Cell">3PM</th>
+                        <td id="15Mo" class="Cell"></td>
+                        <td id="15Tu" class="Cell"></td>
+                        <td id="15We" class="Cell"></td>
+                        <td id="15Th" class="Cell"></td>
+                        <td id="15Fr" class="Cell"></td>
                     </tr>
-                    <tr class="16-Row">
-                        <th class="Day-Row">4PM</th>
-                        <td id="16Mo"></td>
-                        <td id="16Tu"></td>
-                        <td id="16We"></td>
-                        <td id="16Th"></td>
-                        <td id="16Fr"></td>
+                    <tr>
+                        <th class="Header Cell">4PM</th>
+                        <td id="16Mo" class="Cell"></td>
+                        <td id="16Tu" class="Cell"></td>
+                        <td id="16We" class="Cell"></td>
+                        <td id="16Th" class="Cell"></td>
+                        <td id="16Fr" class="Cell"></td>
                     </tr>
-                    <tr class="17-Row">
-                        <th class="Day-Row">5PM</th>
-                        <td id="17Mo"></td>
-                        <td id="17Tu"></td>
-                        <td id="17We"></td>
-                        <td id="17Th"></td>
-                        <td id="17Fr"></td>
+                    <tr>
+                        <th class="Header Cell">5PM</th>
+                        <td id="17Mo" class="Cell"></td>
+                        <td id="17Tu" class="Cell"></td>
+                        <td id="17We" class="Cell"></td>
+                        <td id="17Th" class="Cell"></td>
+                        <td id="17Fr" class="Cell"></td>
                     </tr>
                 </table>
             </div>
